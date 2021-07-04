c871754d8b1b90abe540b0675391a4ae7482a2a2
freezr99779977 <freezr9977@hotmail.com>
2021-07-04 11:55:21 +0800

    chapter4 web form test.

--- app.py
+++ app.py
@@ -2,8 +2,12 @@ from datetime import datetime
 from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = '2211'
 
 bootstrap = Bootstrap(app)
 moment = Moment(app)
@@ -19,13 +23,24 @@ def internal_server_error(e):
     return render_template('404.html'), 500
 
 
-@app.route('/')
+class NameForm(FlaskForm):
+    name = StringField('What is your name?', validators=[DataRequired()])
+    submit = SubmitField('Submit')
+
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html', current_time=datetime.utcnow())
+    name = None
+    form = NameForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ''
+    return render_template('index.html', form=form, name=name, current_time=datetime.utcnow())
 
 
 @app.route('/user/<name>')
 def user(name):
     return render_template('user.html', name=name)
+
 
 app.run()
--- hello.py
+++ hello.py
@@ -7,17 +7,25 @@ from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
 
 app = Flask(__name__)
-app.config['secret_key'] = '1122'
+app.config['SECRET_KEY'] = '1122'
 
 bootstrap = Bootstrap(app)
 moment = Moment(app)
-
 
 
 class NameForm(FlaskForm):
     name = StringField('What is your name?', validators=[DataRequired()])
     submit = SubmitField('Submit')
 
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    name = None
+    form = NameForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ''
+    return render_template('main1.html', form=form, name=name, current_time=datetime.utcnow())
 
 
 @app.errorhandler(404)
@@ -30,13 +38,15 @@ def internal_server_error(e):
     return render_template('404.html'), 500
 
 
-@app.route('/')
-def index():
-    return render_template('index.html', current_time=datetime.utcnow())
+# @app.route('/')
+# def index():
+#     return render_template('index.html', current_time=datetime.utcnow())
 
 
 @app.route('/user/<name>')
 def user(name):
     return render_template('user.html', name=name)
 
-app.run()
+
+if __name__ == '__main__':
+    app.run()
